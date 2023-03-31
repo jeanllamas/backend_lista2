5fe76cebb357915bbc446a0aeacb5232b285ebdb
JeanLlamas <jeanaugustollamas@outlook.com>
2023-03-31 17:27:49 -0300

    exercícios 5 e 6 + reformulação da verificação e escolha do exercício

--- lista2.cs
+++ lista2.cs
@@ -7,13 +7,10 @@ namespace backend_lista2
     {
         static void Main(string[] args)
         {
-        inicio:
-            int resposta = 0;
+            int ligado = 1; //"Liga" o programa
             do
             {
                 Console.Clear();
-                resposta = 0;
-                int escolha;
 
                 Console.WriteLine("Jean Augusto - Lista 2" + Environment.NewLine);
                 for (int i = 1; i <= 15; i++)
@@ -21,276 +18,269 @@ namespace backend_lista2
                     Console.WriteLine($"{i} - Exercício {i}");
                 }
                 Console.WriteLine(Environment.NewLine + "Sair - Encerrar programa");
-                Console.WriteLine(Environment.NewLine + "Selecione um número para ver o exercício (Ex.: '4' para exercício 4):");
-                string verifica = Console.ReadLine();
+                Console.WriteLine(Environment.NewLine + "Selecione um número para acessar o exercício (Ex.: '4' para exercício 4):");
 
-                //Etapa de verificação de escolha
-                if (verifica == "Sair" || verifica == "sair")
-                {
-                    continue; //Sai do do-while encerrando o programa
-                }
-                else if ((verifica.All(Char.IsLetter))) //Verifica se a string contém letra
-                {
-                    goto inicio; //Se houver alguma letra no lugar de um número, exceto as palavras de saída, volta para o começo
-                }
-                else if (!(int.Parse(verifica) >= 1 && int.Parse(verifica) <= 10)) //Verifica se não é menor que 1 e maior que 10
-                {
-                    goto inicio; //Se for um número que não está entre 1 a 10, volta para o começo
-                }
-                else
-                {
-                    escolha = int.Parse(verifica); //Prossegue com a opção válida escolhida
-                }
-                Console.Clear();
+                var entrada = Console.ReadLine();
 
-                //Exercício 1
-                if (escolha == 1)
+                switch (entrada) //Encerra quando o usuário escolher a opção de sair
                 {
-                    Exercicio1();
+                    case "sair":
+                    case "Sair":
+                        ligado = 0;
+                        continue;
                 }
 
-                //Exercício 2
-                else if (escolha == 2)
-                {
-                    Exercicio2();
-                }
+                bool checagem = int.TryParse(entrada, out int num_exercicio); /*Retorna verdadeiro se a conversão da entrada para inteiro der certo
+                                                                                e move o número correspondente ao exercício para uma int */
 
-                //Exercício 3
-                else if (escolha == 3)
+                switch (num_exercicio)
                 {
-                    Exercicio3();
-                }
+                    //Se for um valor fora do limite ou não for um número, retorna uma mensagem de erro
+                    case < 1 when checagem:
+                    case > 15 when checagem:
+                    default:
+                        Console.WriteLine(Environment.NewLine + "Opção de exercício inválida. Tente novamente.");
+                        break;
 
-                //Exercício 4
-                else if (escolha == 4)
-                {
-                    Exercicio4();
-                }
-
-                //Exercício 5
-                else if (escolha == 5)
-                {
-                    Exercicio5();
-                }
-
-                //Exercício 6
-                else if (escolha == 6)
-                {
-                    Exercicio6();
-                }
-
-                //Exercício 7
-                else if (escolha == 7)
-                {
-                    Exercicio7();
-                }
-
-                //Exercício 8
-                else if (escolha == 8)
-                {
-                    Exercicio8();
-                }
-
-                //Exercício 9
-                else if (escolha == 9)
-                {
-                    Exercicio9();
-                }
-
-                //Exercício 10
-                else if (escolha == 10)
-                {
-                    Exercicio10();
-                }
-
-                //Exercício 11
-                else if (escolha == 11)
-                {
-                    Exercicio11();
-                }
-
-                //Exercício 12
-                else if (escolha == 12)
-                {
-                    Exercicio12();
-                }
-
-                //Exercício 13
-                else if (escolha == 13)
-                {
-                    Exercicio13();
-                }
-
-                //Exercício 14
-                else if (escolha == 14)
-                {
-                    Exercicio14();
-                }
-
-                //Exercício 15
-                else if (escolha == 15)
-                {
-                    Exercicio15();
+                    //Quando o valor estiver dentro do limite, segue normalmente
+                    case >= 1 when checagem:
+                    case <= 15 when checagem:
+                        Console.Clear();
+                        Exercicios(num_exercicio); //Encaminha para o exercício escolhido
+                        break;
                 }
 
                 //Mensagem para retornar ou encerrar
                 Console.WriteLine(Environment.NewLine + "Voltar ao início? (S/N)");
-                string encerrar = Console.ReadLine();
-                if (encerrar != "S" && encerrar != "s")
+                var voltar = Console.ReadLine();
+
+                switch (voltar)
                 {
-                    continue;
+                    case "S":
+                    case "s":
+                        continue; //Mantém o programa "ligado"
+                    default:
+                        ligado = 0; //"Desliga" o programa
+                        continue;
                 }
-                else { resposta = 1; }
 
-            } while (resposta != 0);
+            } while (ligado == 1);
         }
 
-        static void Exercicio1()
+        static void Exercicios(int escolha)
         {
-            int numero = 0;
-            int soma_par = 0;
-            int soma_impar = 0;
-            List<int> pares = new List<int>();
-            List<int> impares = new List<int>();
-            
-            Console.WriteLine("Exercício 1 - Soma de números pares e ímpares" + Environment.NewLine);
-
-            while (numero <= 1000)
+            switch(escolha)
             {
-                Console.Write("Digite um número:  ");
-                numero = Convert.ToInt32(Console.ReadLine());
-                Console.Write(Environment.NewLine);
+                case 1: //Exercício 1
+                    {
+                        Console.WriteLine("Exercício 1 - Soma de números pares e ímpares" + Environment.NewLine);
 
-                if (numero % 2 == 0 && numero <= 1000)
-                {
-                    pares.Add(numero);
-                }
-                else if(numero % 2 == 1 && numero <= 1000)
-                {
-                    impares.Add(numero);
-                }
+                        int numero = 0, soma_par = 0, soma_impar = 0;
+
+                        List<int> pares = new List<int>();
+                        List<int> impares = new List<int>();
+
+                        while (numero <= 1000)
+                        {
+                            Console.Write("Digite um número:  ");
+                            numero = Convert.ToInt32(Console.ReadLine());
+                            Console.Write(Environment.NewLine);
+
+                            if (numero % 2 == 0 && numero <= 1000)
+                            {
+                                pares.Add(numero);
+                            }
+                            else if (numero % 2 == 1 && numero <= 1000)
+                            {
+                                impares.Add(numero);
+                            }
+                        }
+                        foreach (int par in pares)
+                        {
+                            soma_par += par;
+                        }
+                        foreach (int impar in impares)
+                        {
+                            soma_impar += impar;
+                        }
+                        Console.WriteLine($"Soma de números pares: {soma_par}" + Environment.NewLine);
+                        Console.WriteLine($"Soma de números ímpares: {soma_impar}");
+                        break;
+                    }
+
+                case 2: //Exercício 2
+                    {
+                        Console.WriteLine("Exercício 2 - N números pares e ímpares" + Environment.NewLine);
+
+                        int n, pares = 0, impares = 1;
+
+                        Console.Write("Digite um número: ");
+                        n = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(Environment.NewLine + $"Primeiros {n} números pares");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(pares + " ");
+                            pares += 2;
+                        }
+
+                        Console.WriteLine(Environment.NewLine + Environment.NewLine + $"Primeiros {n} números ímpares");
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write(impares + " ");
+                            impares += 2;
+                        }
+
+                        Console.WriteLine();
+                        break;
+                    }
+
+                case 3: //Exercício 3
+                    {
+                        Console.WriteLine("Exercício 3 - Produto dos números" + Environment.NewLine);
+
+                        int numero = 1, produto = 1;
+
+                        while (numero != 0)
+                        {
+                            Console.Write("Digite um número:  ");
+                            numero = Convert.ToInt32(Console.ReadLine());
+
+                            if (numero > 0)
+                            {
+                                produto *= numero;
+                                Console.WriteLine(produto + Environment.NewLine);
+                            }
+                        }
+                        break;
+                    }
+
+                case 4: //Exercício 4
+                    {
+                        Console.WriteLine("Exercício 4 - Tabuada de 0 a 9" + Environment.NewLine);
+
+                        int num;
+
+                        Console.WriteLine("Digite um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(Environment.NewLine + $"Tabuada do {num}" + Environment.NewLine);
+                        for (int i = 0; i <= 9; i++)
+                        {
+                            Console.WriteLine($"{num} * {i} = {num * i}");
+                        }
+                        break;
+                    }
+
+                case 5: //Exercício 5
+                    {
+                        Console.WriteLine("Exercício 5 - Maior e menor número" + Environment.NewLine);
+
+                        int[] numeros = new int[15];
+                        int maior, menor;
+
+                        for (int i = 0; i < 15; i++) //Recebimento dos 15 valores
+                        {
+                            Console.Write($"Digite o {i + 1}º número: "); //i+1 para corrigir visualmente o 1º espaço da array numero[0]
+                            numeros[i] = (Convert.ToInt32(Console.ReadLine()));
+                        }
+
+                        maior = numeros[0];
+                        menor = numeros[0];
+
+                        for (int i = 1; i < 15; i++) //Etapa de verificação do maior e menor número
+                        {
+                            if (numeros[i] > maior) //Atualiza qual é o maior e menor número conforme avança posições na array
+                            {
+                                maior = numeros[i];
+                            }
+                            else if (numeros[i] < menor)
+                            {
+                                menor = numeros[i];
+                            }
+                        }
+
+                        Console.WriteLine(Environment.NewLine + $"O maior número digitado foi {maior} e o menor {menor}");
+                        break;
+                    }
+
+                case 6: //Exercício 6
+                    {
+                        int idade = 0;
+                        List<int> menores21 = new List<int>();
+                        List<int> maiores50 = new List<int>();
+
+                        Console.WriteLine("Exercício 6 - Pessoas com menos de 21 e mais de 50 anos" + Environment.NewLine);
+
+                        do
+                        {
+                            Console.Write("Digite a idade de uma pessoa: ");
+                            idade = Convert.ToInt32(Console.ReadLine());
+
+                            if (idade < 21)
+                            {
+                                menores21.Add(idade);
+                            }
+                            else if (idade > 50)
+                            {
+                                maiores50.Add(idade);
+                            }
+
+                        } while (idade != -99);
+
+                        Console.WriteLine(Environment.NewLine + "Total:");
+                        Console.WriteLine($"Menos de 21 anos: {menores21.Count} pessoas");
+                        Console.WriteLine($"Mais de 50 anos: {maiores50.Count} pessoas");
+
+                        break;
+                    }
+
+                case 7: //Exercício 7
+                    {
+                        break;
+                    }
+
+                case 8: //Exercício 8
+                    {
+                        break;
+                    }
+
+                case 9: //Exercício 9
+                    {
+                        break;
+                    }
+
+                case 10: //Exercício 10
+                    {
+                        break;
+                    }
+
+                case 11: //Exercício 11
+                    {
+                        break;
+                    }
+
+                case 12: //Exercício 12
+                    {
+                        break;
+                    }
+
+                case 13: //Exercício 13
+                    {
+                        break;
+                    }
+
+                case 14: //Exercício 14
+                    {
+                        break;
+                    }
+
+                case 15: //Exercício 15
+                    {
+                        break;
+                    }
+
             }
-            foreach (int par in pares)
-            {
-                soma_par += par;
-            }
-            foreach (int impar in impares)
-            {
-                soma_impar += impar;
-            }
-            Console.WriteLine($"Soma de números pares: {soma_par}" + Environment.NewLine);
-            Console.WriteLine($"Soma de números pares: {soma_impar}");
         }
-
-        static void Exercicio2()
-        {
-            int n, pares = 0, impares = 1;
-
-            Console.WriteLine("Exercício 2 - N números pares e ímpares" + Environment.NewLine);
-
-            Console.Write("Digite um número: ");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(Environment.NewLine + $"Primeiros {n} números pares");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(pares + " ");
-                pares += 2;
-            }
-
-            Console.WriteLine(Environment.NewLine + Environment.NewLine + $"Primeiros {n} números ímpares");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write(impares + " ");
-                impares += 2;
-            }
-
-            Console.WriteLine();
-        }
-
-        static void Exercicio3()
-        {
-            int numero = 1, produto = 1;
-
-            Console.WriteLine("Exercício 3 - Produto dos números" + Environment.NewLine);
-
-            while (numero != 0)
-            {
-                Console.Write("Digite um número:  ");
-                numero = Convert.ToInt32(Console.ReadLine());
-
-                if (numero > 0)
-                {
-                    produto *= numero;
-                    Console.WriteLine(produto + Environment.NewLine);
-                }
-            }
-        }
-
-        static void Exercicio4()
-        {
-            int num;
-
-            Console.WriteLine("Exercício 4 - Tabuada de 0 a 9" + Environment.NewLine);
-
-            Console.WriteLine("Digite um número: ");
-            num = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(Environment.NewLine + $"Tabuada do {num}");
-            for (int i = 0; i <= 9; i++)
-            {
-                Console.WriteLine($"{num} * {i} = {num * i}");
-            }
-        }
-
-        static void Exercicio5()
-        {
-            Console.WriteLine("Exercício 5 - Maior e menor número" + Environment.NewLine);
-        }
-
-        static void Exercicio6()
-        {
-            
-        }
-
-        static void Exercicio7()
-        {
-        }
-
-        static void Exercicio8()
-        {
-            
-        }
-
-        static void Exercicio9()
-        {
-            
-        }
-
-        static void Exercicio10()
-        {
-
-        }
-        static void Exercicio11()
-        {
-
-        }
-        static void Exercicio12()
-        {
-
-        }
-        static void Exercicio13()
-        {
-
-        }
-        static void Exercicio14()
-        {
-
-        }
-        static void Exercicio15()
-        {
-
-        }
-
     }
 }
